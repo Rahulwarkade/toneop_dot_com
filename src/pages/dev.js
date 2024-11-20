310a3e61ae8630a1332f87145d7d63e3477310bd
Rahul Warkade <rahulwarkade954@gmail.com>
2024-11-20 09:46:53 +0530

    Gallery

---
+++ src/pages/dev.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Gallery} from '@/components/Gallery';
+
+export default function Dev(){
+    return (
+        <>
+            <section className="w-full h-screen bg-gradient-to-r from-[#EFCAA7] to-[#8DABD1] flex items-center justify-center">
+                <Gallery/>
+            </section>
+        </>
+    )
+}
