6226634d2342c7496eacebb44a08ae06bae431f4
Rahul Warkade <rahulwarkade954@gmail.com>
2024-11-21 09:58:37 +0530

    adding

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,8 +7,8 @@ const Gallery = () => {
   return (
     <>
       <section className="w-full flex items-center justify-end gap-10">
-        <div className="w-[261px] h-[654px] hover:w-[551px] flex items-center justify-end relative duration-500 ease-linear">
-          <div  className="w-[261px] h-[654px] hover:w-[551px] duration-500 ease-linear rounded-[20px] overflow-hidden absolute  right-0">
+        <div className="w-[261px] h-[654px] hover:w-[551px] flex items-center justify-end relative duration-300 ease-out ">
+          <div  className="w-[261px] h-[654px] hover:w-[551px] duration-300 ease-out rounded-[20px] overflow-hidden absolute  right-0">
             <Image 
               src={kikuCare}
               fill
@@ -18,12 +18,12 @@ const Gallery = () => {
               style={{
                 objectFit:"cover"
               }}
-              className="hover:scale-110 duration-500 ease-linear"
+              className="hover:scale-110 duration-300 ease-out"
             />
           </div>
           </div>
-          <div className="w-[261px] h-[654px] hover:w-[551px] bg-[#F9F9F9] flex items-center justify-end relative duration-500 ease-linear rounded-[20px] overflow-hidden">
-          <div  className="w-[261px] h-[654px] hover:w-[551px] duration-500 ease-linear rounded-[20px] overflow-hidden absolute  right-0">
+          <div className="w-[261px] h-[654px] hover:w-[551px] bg-[#F9F9F9]  flex items-center justify-end relative duration-300 ease-out rounded-[20px] overflow-hidden">
+          <div  className="w-[261px] h-[654px] hover:w-[551px] duration-300 ease-out rounded-[20px] overflow-hidden absolute  right-0">
             <Image 
               src={raghavEats}
               fill
@@ -33,12 +33,13 @@ const Gallery = () => {
               style={{
                 objectFit:"cover"
               }}
-              className="hover:scale-110 duration-500 ease-linear"
+              className="hover:scale-110 duration-300 ease-out"
             />
+            <span className="w-[87px] h-[187px] absolute bg-[#ffffff91] bottom-5  "></span>
           </div>
               </div>
-              <div className="w-[261px] h-[654px] hover:w-[551px] duration-500 ease-linear flex items-center justify-end relative">
-          <div  className="w-[261px] h-[654px] hover:w-[551px] duration-500 ease-linear rounded-[20px] overflow-hidden absolute  right-0">
+              <div className="w-[261px] h-[654px] hover:w-[551px] duration-300 ease-out flex items-center justify-end relative">
+          <div  className="w-[261px] h-[654px] hover:w-[551px] duration-300 ease-out rounded-[20px] overflow-hidden absolute  right-0">
             <Image 
               src={milindFit}
               fill
@@ -47,7 +48,7 @@ const Gallery = () => {
               style={{
                 objectFit:"cover"
               }}
-              className="hover:scale-110 duration-500 ease-linear"
+              className="hover:scale-110 duration-300 ease-out"
             />
           </div>
           </div>
